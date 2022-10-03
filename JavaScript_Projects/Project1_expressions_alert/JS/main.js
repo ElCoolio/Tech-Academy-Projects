69ffc22912b87ac80d83b3f5d0a69eff3ff02b7b
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2022-10-03 14:32:15 -0400

    prep b4 submission

--- JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,3 @@
-var A = "This is a string!"
-document.write(A);
+var A = "This is the begining of the string", B = " and this is the end of the string"  //Declaring two Stirngs
+document.write(3+3); // displaying the solution to a simple math expression
+window.alert(A + B); // on website load posting an alert window that concatenates and displays the two strings
