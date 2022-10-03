f90e1f5b590114696dae27257383a6f5236d7bbe
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2022-10-03 14:56:56 -0400

    Code for assignment #2

--- JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,7 @@
 var A = "This is the begining of the string", B = " and this is the end of the string"  //Declaring two Stirngs
 document.write(3+3); // displaying the solution to a simple math expression
 window.alert(A + B); // on website load posting an alert window that concatenates and displays the two strings
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+  }
---
+++ JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function displayDate() { //utalizes get ElementByID to display the current date and time.
+    document.getElementById("demo").innerHTML = Date();
+  }
+
+  function displayGreeting() { //utilizes get ElementByID to display a simple string using operators
+    var sentence = "Hello World!";
+    sentence += " It is nice to meet you!";
+    document.getElementById("demo2").innerHTML = sentence;
+  }
