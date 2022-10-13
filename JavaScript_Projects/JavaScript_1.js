240370f77bf87af387ea088aadb8787ed3f456a1
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2022-10-13 09:32:37 -0400

    Code for assignment

---
+++ JavaScript_Projects/JavaScript_1.js
@@ -0,0 +1,30 @@
+function textChange(){
+    var A = document.getElementsByClassName("click");
+    A[0].innerHTML = "The test has changed";
+}
+
+function DrawCanvas(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.font = "72px Arial";
+    ctx.strokeText("Hello World", 0, 125);
+}
+
+function DrawCanvas2(){
+    var c = document.getElementById("myCanvas2");
+    var ctx = c.getContext("2d");
+    var grd = ctx.createLinearGradient(0, 0, 500, 0);
+    grd.addColorStop(0, "black");
+    grd.addColorStop(1, "white");
+    
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, 500, 250);
+}
+
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
